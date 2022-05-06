90bd24b35df6eaf4c121f621718dcdf8d0c5fea2
Michael Alejandro Rios Rodriguez <71363790+MCLRDRI15@users.noreply.github.com>
2022-05-06 09:48:50 -0500

    Mejora de selectores

--- cypress/page/login.page.ts
+++ cypress/page/login.page.ts
@@ -5,8 +5,8 @@ class LoginPage {
   private loginButton: string;
 
   constructor() {
-    this.emailType = "#email";
-    this.passwordType = "#passwd";
+    this.emailType = "form div.form-group > input#email";
+    this.passwordType = "form div.form-group > span input#passwd";
     this.loginButton = "div.form_content button span i.icon-lock";
   }
 
