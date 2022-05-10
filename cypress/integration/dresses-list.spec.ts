5819c2b43e66c3ea4ca246d2385bece788581438
Michael Alejandro Rios Rodriguez <71363790+MCLRDRI15@users.noreply.github.com>
2022-05-10 15:12:30 -0500

    Update all test with AAA fixes

--- cypress/integration/dresses-list.spec.ts
+++ cypress/integration/dresses-list.spec.ts
@@ -3,19 +3,18 @@ import {MenuContentPage, DressesListPage} from "../page/index";
 describe("the user navigates to the dresses page should", () => {
   let menuContentPage: MenuContentPage;
   let dressesListPage: DressesListPage;
+  let allDressesNames: string[];
 
   before(() => {
     menuContentPage = new MenuContentPage();
     dressesListPage = new DressesListPage();
+    allDressesNames =
+     ["Printed Dress", "Printed Dress", "Printed Summer Dress", "Printed Summer Dress", "Printed Chiffon Dress"];
   });
 
   it("show the available dresses", () => {
-    // ... realiza la prueba
-
     // Arrange
     menuContentPage.visitMenuContentPage();
-    const allDressesNames =
-     ["Printed Dress", "Printed Dress", "Printed Summer Dress", "Printed Summer Dress", "Printed Chiffon Dress"];
 
     // Act
     menuContentPage.goToDressesMenu();
