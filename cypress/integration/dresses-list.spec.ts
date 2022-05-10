999f9cf391d4cb5c70a9c58acddffd125360e71e
Michael Alejandro Rios Rodriguez <71363790+MCLRDRI15@users.noreply.github.com>
2022-05-10 11:13:31 -0500

    AAA pattern update for Dresses and Tshirts

--- cypress/integration/dresses-list.spec.ts
+++ cypress/integration/dresses-list.spec.ts
@@ -1,15 +1,28 @@
-// import {MenuContentPage, DressesListPage} from "../page/index";
+import {MenuContentPage, DressesListPage} from "../page/index";
 
-// describe("the user navigates to the dresses page should", () => {
-//   let menuContentPage: MenuContentPage;
-//   let dressesListPage: DressesListPage;
+describe("the user navigates to the dresses page should", () => {
+  let menuContentPage: MenuContentPage;
+  let dressesListPage: DressesListPage;
 
-//   before(() => {
-//     menuContentPage = new MenuContentPage();
-//     dressesListPage = new DressesListPage();
-//   });
+  before(() => {
+    menuContentPage = new MenuContentPage();
+    dressesListPage = new DressesListPage();
+  });
 
-//   it("show the available dresses", () => {
-//     // ... realiza la prueba
-//   });
-// });
+  it("show the available dresses", () => {
+    // ... realiza la prueba
+
+    // Arrange
+    menuContentPage.visitMenuContentPage();
+    const allDressesNames =
+     ["Printed Dress", "Printed Dress", "Printed Summer Dress", "Printed Summer Dress", "Printed Chiffon Dress"];
+
+    // Act
+    menuContentPage.goToDressesMenu();
+    dressesListPage.getDressProducts();
+
+    // Assert
+    dressesListPage.validateItemsNumber(allDressesNames.length);
+    dressesListPage.validateItemsNames(allDressesNames);
+  });
+});
