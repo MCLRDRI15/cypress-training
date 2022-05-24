1fae9774e7a984a0157f26f4bc92e710ce53827d
Michael Alejandro Rios Rodriguez <71363790+MCLRDRI15@users.noreply.github.com>
2022-05-24 17:11:06 -0500

    fixes

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -26,7 +26,6 @@
 
 import "cypress-file-upload";
 import "cypress-iframe";
-require("cypress-iframe");
 
 // Ignoring uncaught exceptions since errors from external apps should not stop de workshop
 Cypress.on("uncaught:exception", (err, runnable) => {
